a99c86ce3bb66b3c63a05a9def11e3182b7ed34c
dengbo <dengbo@dengbo.local>
2021-11-25 21:55:52 +0800

    add: 1,2,3,4,5,6,7,8
    add: 2
    
    add: 3
    
    add: 4
    
    add: 5
    
    add: 6,7
    
    add: 7
    
    add: 8

--- src/pages/login/test.tsx
+++ src/pages/login/test.tsx
@@ -6,6 +6,14 @@ const Test = () => {
   }, []);
 
   cb(1, 2);
+  console.log(1);
+  console.log(2);
+  console.log(3);
+  console.log(4);
+  console.log(5);
+  console.log(6);
+  console.log(7);
+  console.log(8);
 
   return <div>test</div>;
 };
